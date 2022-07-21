a524b2834e18e80c4bd36e2a8ae68d7bcc6b7872
ej8899 <ej8899@gmail.com>
2022-07-20 20:49:49 -0600

    basic outline

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+//
+// typewriter.js
+// https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w4/activities/387?journey_step=32&workbook=7
+// 
+
+
+
+const sentence = "hello there from lighthouse labs";
+
+for (const char of sentence) {
+  let counter=1;
+  process.stdout.write(char);
+
+
+}
