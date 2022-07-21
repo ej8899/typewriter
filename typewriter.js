d3d9ed74558a64fee025934bd0d9484ee91b9ee0
ej8899 <ej8899@gmail.com>
2022-07-20 21:29:51 -0600

    more speed adjust + eslint

--- typewriter.js
+++ typewriter.js
@@ -10,21 +10,18 @@ const quotesArray = [
   'Who is the more foolish: the fool or the fool who follows him?',
   'Your focus determines your reality.', 'The ability to speak does not make you intelligent.', 'Many of the truths that we cling to depend on our viewpoint.', 'I like firsts. Good or bad, they are always memorable.', 'In my experience, there is no such thing as luck.', 'A friendly piece of advice; assume that I know everything.'];
 
-let quoteNumber = Math.floor((Math.random() * quotesArray.length) + 1);
+let quoteNumber = Math.floor((Math.random() * quotesArray.length-1) + 1);
 
 //const sentence = "hello there from lighthouse labs";
 const sentence = quotesArray[quoteNumber];
 
 const outputSentence = conColorGreen + sentence + conColorReset + '\n';  // Fix the new line "bug" with \n
 
-let timer = 50, timerIncrement = 50;
+let timer = 50;
 for (const char of outputSentence) {
-  let counter=1;
-  let timerIncrement = Math.floor((Math.random() * 170) + 35); // simulate a 'slow-ish' typer with these numbers
+  let timerIncrement = Math.floor((Math.random() * 160) + 25); // simulate a 'slow-ish' typer with these numbers
   setTimeout(() => {
     process.stdout.write(`${char}`);
   }, timer);
-  
-
   timer = timer + timerIncrement;
 }
